b661a6a89552b5dcb570bdefe4a89326ad02cd18
cau1k <zero@composer.group>
2025-12-02 00:25:26 -0500

    feat: awesome

--- apps/docs/src/app/docs/layout.tsx
+++ apps/docs/src/app/docs/layout.tsx
@@ -1,10 +1,10 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from '@/components/layout/docs';
-import { baseOptions } from '@/lib/layout.shared';
+import { source } from "@/lib/source";
+import { DocsLayout } from "@/components/layout/docs";
+import { baseOptions } from "@/lib/layout.shared";
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
+export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout tree={source.pageTree} tabMode="auto" {...baseOptions()}>
       {children}
     </DocsLayout>
   );
